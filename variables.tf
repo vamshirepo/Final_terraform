3dba26f6ea4f66ce33bfe214f7ea5bdfdfbc58d5
Rakesh-k-ops <rakeshkdevops001@gmail.com>
2025-03-12 22:20:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,27 +14,32 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
+
 variable "app_asg" {
-  description = "auto scaling group"
-  type	     = string
-  default    = "lt-063d5ac86004626fe"
+  description = "Auto Scaling Group"
+  type        = string
+  default     = "lt-063d5ac86004626fe"
 }
+
 variable "vpc_id" {
-  description = "for vpc id"
-  type       = string
-  default    = "vpc-06ae7c96727ba17f8"
+  description = "VPC ID"
+  type        = string
+  default     = "vpc-06ae7c96727ba17f8"
 }
+
 variable "subnet_1_id" {
-  description = "for subnet 1 id"
-  type       = string  
-  default    = "subnet-0739686a07685b631"
+  description = "Subnet 1 ID"
+  type        = string  
+  default     = "subnet-0739686a07685b631"
 }
+
 variable "subnet_2_id" {
-  description = "for subnet 2 id"
-  type       = string  
-  default    = "subnet-0472fa04e525a0a69"
+  description = "Subnet 2 ID"
+  type        = string  
+  default     = "subnet-0472fa04e525a0a69"
 }
+
 variable "key_name" {
   description = "The name of the SSH key pair"
-  type        = string
+  type        = string  
 }
